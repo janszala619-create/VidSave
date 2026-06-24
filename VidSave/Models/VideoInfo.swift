1b506dbb4089048b61cd30ba663d70e4b1bdb89f
Janek_46 <jan.szala619@yahoo.de>
2026-06-23 20:31:17 -0700

    Add VidSave/Models/VideoInfo.swift

---
+++ VidSave/Models/VideoInfo.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+struct VideoInfo: Identifiable, Codable {
+    let id: UUID
+    let title: String
+    let thumbnailURL: String
+    let sourceURL: String
+    let formats: [VideoFormat]
+
+    init(id: UUID = UUID(), title: String, thumbnailURL: String, sourceURL: String, formats: [VideoFormat]) {
+        self.id = id
+        self.title = title
+        self.thumbnailURL = thumbnailURL
+        self.sourceURL = sourceURL
+        self.formats = formats
+    }
+}
+
+struct VideoFormat: Identifiable, Codable {
+    let id: String
+    let label: String
+    let ext: String
+}
